343c171049e5a6590f9486af49afc72a5f55489c
Benedikt Schöffmann <benedikt.schoeffmann@gmail.com>
2021-01-23 20:48:40 +0100

    Add: Blinkenlights with a Display

---
+++ HD47780 Display/HD47780/HD47780.ino
@@ -0,0 +1,46 @@
+/*
+ * This sketch exists because I wnat to know, how 
+ * the stuff with the LCDs worlks. 
+ * 
+ * Also _some_ visual hints when debugging would be nice D:
+ */
+
+#include <LiquidCrystal.h>
+
+#ifndef HD47780_INO
+#define HD47780_INO
+
+#define PIN_LCD_RS 12             // Register Select
+#define PIN_LCD_EN  11            // Enable
+#define PIN_LCD_D4 5              // Bit 4
+#define PIN_LCD_D5 4              // Bit 5
+#define PIN_LCD_D6 3              // Bit 6
+#define PIN_LCD_D7 2              // Bit 7
+
+#define LCD_ROWS 2                // Rows
+#define LCD_COLS 16               // Columns
+
+
+LiquidCrystal lcd(PIN_LCD_RS, PIN_LCD_EN, PIN_LCD_D4, PIN_LCD_D5, PIN_LCD_D6, PIN_LCD_D7);
+byte i = 0, y = 0, x = 0;
+
+void setup() {
+  lcd.begin(LCD_COLS, LCD_ROWS);  
+  lcd.clear();
+  Serial.begin(9600);
+}
+
+void loop() {
+  i = i % 32;
+
+  y = (i < 16) ? 0 : 1;
+  x = abs((-y*16) + i % 16);
+  lcd.setCursor(x, y);
+  lcd.write('*');
+  delay(100);
+  lcd.setCursor(x, y);
+  lcd.write(' ');
+  i++;
+}
+
+#endif
