c49ef1df3db1dbaf5e534886fa8bc38b23c4fd4f
Benedikt Schöffmann <benedikt.schoeffmann@gmail.com>
2021-01-24 15:44:37 +0100

    Add: intervall definition for modes

---
+++ scales/scales.h
@@ -0,0 +1,20 @@
+#ifndef SCALES_H_
+#define SCALES_H_
+
+    const int dorian[7]= {2, 1, 2, 2, 2, 1, 2};
+
+    const int phrygian[7]= {1, 2, 2, 2, 1, 2, 2};
+
+    const int lydian[7]= {2, 2, 2, 1, 2, 2, 2};
+
+    const int mixolydian[7]= {2, 2, 1, 2, 2, 1, 2};
+
+    const int lydian[7]= {2, 2, 2, 1, 2, 2, 1};
+
+    const int aeoian[7]= {2, 1, 2, 2, 2, 1, 2};
+
+    const int locrian[7]= {1, 2, 2, 1, 2, 2, 2};
+
+    const int* scales={dorian, phrygian, lydian, mixolydian, lydian, aeolian, locrian}
+
+#endif
